26157bdd1b523924fc43e5f93c8576cf75748d66
zainulfakhri16 <zainul.fakhri2002@gmail.com>
2022-10-23 01:56:30 +0800

    sg main

---
+++ app/src/main/java/com/example/instagram/ItemsViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.instagram
+
+import android.widget.EditText
+
+data class ItemsViewModel (val image:Int, val text: String) {
+}
--- app/src/main/java/com/example/instagram/MainActivity.kt
+++ app/src/main/java/com/example/instagram/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.instagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val movieList = ArrayList<MovieModel>()
+    private lateinit var moviesAdapter: MovieAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title ="Instagram"
+        val recyclerView = findViewById<RecyclerView>(R.id.rv_sw)
+        moviesAdapter = MovieAdapter(movieList)
+        val mLayoutManager = LinearLayoutManager (applicationContext)
+        mLayoutManager.orientation =LinearLayoutManager.HORIZONTAL
+        recyclerView.layoutManager = mLayoutManager
+        recyclerView.itemAnimator = DefaultItemAnimator()
+        recyclerView.adapter = moviesAdapter
+        prepare
     }
 }
